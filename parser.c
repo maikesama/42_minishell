59ecd1ad8e217d982524aa202f7ae46824342d5f
Michele Paci <mpaci@e4r3p4.42roma.it>
2021-12-16 18:32:08 +0100

    pd

--- parser.c
+++ parser.c
@@ -3,143 +3,90 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pceccoli <pceccoli@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mpaci <mpaci@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 15:00:00 by pceccoli          #+#    #+#             */
-/*   Updated: 2021/12/14 18:43:59 by pceccoli         ###   ########.fr       */
+/*   Updated: 2021/12/16 14:28:38 by mpaci            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-void	jeyer(char	*str, int i, int *j, char spec_char)
+int	how_many_spaces(t_all *all)
 {
-	if (str[i + 1] == spec_char)
-	{
-		if (str[i - 1] != ' ')
-			*j += 1;
-		if (str[i + 2] != ' ')
-			*j += 1;
-		i++;
-	}
-	else
-	{
-		if (str[i - 1] != ' ')
-			*j += 1;
-		if (str[i + 1] != ' ')
-			*j += 1;
-	}
-}
-
-int	add_spaces(char *str, char spec_char)
-{
-	int i;
+	int	i;
 	int	j;
-
-	i = 0;
-	j = 0;
-	while(str[i])
-	{
-		if (str[i] == spec_char)
-			jeyer(str, i, &j, spec_char);
-		i++;
-	}
-	return (j);
-}
-
-int is_spec_char(char c)
-{
-	int i;
-	char *specs;
-
-	specs = "<>|";
-	i = 0;
-	while (specs[i])
-	{
-		if (c == specs[i])
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-void	copy_spc(t_all *all, char *tmp) //copy + spaces = new_input;
-{
-	int 	i;
-	int		j;
 
 	i = 0;
 	j = 0;
 	while (all->input[i])
 	{
-		if (!is_spec_char(all->input[i]))
-		{
-			if (all->input[i + 1] == all->input[i])
-			{
-				if (all->input[i - 1] != ' ')
-				{
-					tmp[j] = ' ';
-					j++;
-				}
-				tmp[j] = all->input[i];
-				j++;
-				tmp[j] = all->input[i + 1];
-				j++;
-				if (all->input[i + 2] != ' ')
-				{
-					tmp[j] = ' ';
-					j++;
-				}
-				i++;
-			}
-			else
-			{
-				if (all->input[i - 1] != ' ' && all->input[i - 1])
-				{
-					tmp[j] = ' ';
-					j++;
-				}
-				tmp[j] = all->input[i];
-				j++;
-				if (all->input[i + 1] != ' ' && all->input[i + 1])
-				{
-					tmp[j] = ' ';
-					j++;
-				}
-			}
-			i++;
-		}
-		tmp[j++] = all->input[i++];
+		if (all->input[i] == '\'')
+			all->flag1 *= -1;
+		if (all->input[i] == '"')
+			all->flag2 *= -1;
+		if (all->input[i] == ' ' && all->flag1 == 1 && all->flag2 == 1)
+			j++;
+		i++;
 	}
+	all->flag1 = 1;
+	all->flag2 = 1;
+	return (j);
 }
 
-void	new_input(t_all *all)
+char	*charjoin(char	*s, char c, int max)
 {
-	char	*check;
-	char	*tmp;
-	int		j;
+	char	*ret;
 	int		i;
 
+	ret = malloc(max + 2);
+	i = -1;
+	while (s[++i])
+		ret[i] = s[i];
+	ret[i++] = c;
+	ret[i] = 0;
+	free(s);
+	return (ret); 
+}
+
+void	input_tok(t_all *all)
+{
+	int	j;
+	int	i;
+	int	len;
+	int	start;
+
+	len = 0;
 	i = 0;
-	check = "<>|";
+	start = 0;
+	all->tok = (char **)ft_calloc(how_many_spaces(all) + 2, sizeof(char *));
 	j = 0;
-	while (check[j])
+	while (all->input[i] && all->input)
 	{
-		i += add_spaces(all->input, check[j]);
-		j++;
+		while (all->input[i] != ' ' && all->input[i])
+		{
+			if (all->input[i] == '\'')
+				all->flag1 *= -1;
+			if (all->input[i] == '"')
+				all->flag2 *= -1;
+			len++;
+			i++;
+		}
+		if ((all->input[i] == '\0' || all->input[i] == ' ') && all->flag1 == 1 && all->flag2 == 1)
+		{
+			all->tok[j] = (char *)ft_calloc(len + 1, 1);
+			while (start < (start + len) && all->input[start])
+			{
+				all->tok[j][start] = all->input[start];
+				start++;
+			}
+			j++;
+			start++;
+			len = 0;
+			ft_printf("%d\n",j);
+		}
+		i++;
 	}
-	tmp = ft_calloc(ft_strlen(all->input) + i + 1, 1);
-	copy_spc(all, tmp);
-	all->input = ft_realloc(all->input, ft_strlen(tmp) + 1);
-	all->input[ft_strlen(tmp) + 1] = 0;
-	j = 0;
-	while (tmp[j])
-	{
-		all->input[j] = tmp[j];
-		j++;
-	}
-	free(tmp);
-	printf("%s\n", all->input);
 }
 
 int	parser(t_all *all)
@@ -149,7 +96,12 @@ int	parser(t_all *all)
 	i = 0;
 	if (!check_quotes(all->input) || !check_special(all->input))
 		return (0);
-
-	new_input(all);
-	return (1);	
+	new_input(all);//add delete spaces if more than 1 && add spaces for "" and  '';
+	input_tok(all);//debug
+	while (all->tok[i])
+	{
+		printf("%s\n", all->tok[i]);
+		i++;
+	}
+	return (1);
 }
---
+++ parser_ut2.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_ut2.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpaci <mpaci@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/16 14:18:35 by mpaci             #+#    #+#             */
+/*   Updated: 2021/12/16 14:27:38 by mpaci            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+int	is_spec_char(char c)
+{
+	int		i;
+	char	*specs;
+
+	specs = "<>|";
+	i = 0;
+	while (specs[i])
+	{
+		if (c == specs[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	copy_spc_double(t_all *all, char **tmp, int *i, int *j)
+{
+	if (all->input[(*i) - 1] != ' ' && all->input[*i - 1])
+	{
+		tmp[0][*j] = ' ';
+		*j += 1;
+	}
+	tmp[0][*j] = all->input[*i];
+	*j += 1;
+	tmp[0][*j] = all->input[*i + 1];
+	*j += 1;
+	if (all->input[*i + 2] != ' ' && all->input[*i + 2])
+	{
+		tmp[0][*j] = ' ';
+		*j += 1;
+	}
+	*i += 1;
+}
+
+void	copy_spc_ut(t_all *all, char **tmp, int *i, int *j)
+{
+	if (all->input[*i - 1] != ' ' && all->input[*i - 1])
+	{
+		tmp[0][*j] = ' ';
+		*j += 1;
+	}
+	tmp[0][*j] = all->input[*i];
+	*j += 1;
+	if (all->input[*i + 1] != ' ' && all->input[*i + 1])
+	{
+		tmp[0][*j] = ' ';
+		*j += 1;
+	}
+}
+
+void	copy_spc(t_all *all, char *tmp)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (all->input[i])
+	{
+		if (!is_spec_char(all->input[i]))
+		{
+			if (all->input[i + 1] == all->input[i])
+				copy_spc_double(all, &tmp, &i, &j);
+			else
+				copy_spc_ut(all, &tmp, &i, &j);
+			i++;
+		}
+		tmp[j++] = all->input[i++];
+	}
+}
+
+void	new_input(t_all *all)
+{
+	char	*check;
+	char	*tmp;
+	int		j;
+	int		i;
+
+	i = 0;
+	check = "<>|";
+	j = 0;
+	while (check[j])
+	{
+		i += add_spaces(all->input, check[j]);
+		j++;
+	}
+	tmp = ft_calloc(ft_strlen(all->input) + i + 1, 1);
+	copy_spc(all, tmp);
+	all->input = ft_realloc(all->input, ft_strlen(tmp) + 1);
+	all->input[ft_strlen(tmp) + 1] = 0;
+	j = 0;
+	while (tmp[j])
+	{
+		all->input[j] = tmp[j];
+		j++;
+	}
+	free(tmp);
+}
