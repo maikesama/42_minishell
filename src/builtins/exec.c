93e28e82930a96aa7e1612faa25ad78842b80eda
Michele Paci <michele@MacBook-Air.local>
2022-02-13 17:54:12 +0100

    add make

--- src/builtins/exec.c
+++ src/builtins/exec.c
@@ -36,7 +36,7 @@ void	do_your_job_son(t_all *all, int i, int err)
 	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
 	if (all->tok && all->tok[0] && all->tok[0][0] && ((all->tok[0][0] == '.'
-		&& all->tok[0][1] == '/') || all->tok[0][0] == '/'))
+		&& all->tok[0][1] == '/') || all->tok[0][0] == '/' || !ft_strncmp(all->tok[0], "make", ft_strlen(all->tok[0]))))
 	{
 		all->cmd = ft_calloc(ft_strlen(all->tok[0]) + 1, 1);
 		ft_memcpy(all->cmd, all->tok[0], ft_strlen(all->tok[0]));
